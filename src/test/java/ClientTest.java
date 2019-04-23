2fb356e0c21ef7f63bdc71c30cdeabfb4874afdc
wolf <billodida420@gmail.com>
2019-04-23 20:48:13 +0300

    creating a test to list all objects

--- src/test/java/ClientTest.java
+++ src/test/java/ClientTest.java
@@ -1,5 +1,6 @@
 import org.junit.*;
 import static org.junit.Assert.*;
+import java.time.LocalDateTime;
 
 public class ClientTest {
 
@@ -8,10 +9,23 @@ public class ClientTest {
         Client myClient = new Client("Drew");
         assertEquals(true, myClient instanceof Client);
     }
+
     @Test
     public void Client_instantiatesWithDescription_String() {
         Client myClient = new Client("Drew Stephens");
         assertEquals("Drew Stephens", myClient.getDescription());
     }
 
+    @Test
+    public void getCreatedAt_instantiatesWithCurrentTime_today() {
+        Client myClient = new Client("ken Haj");
+        assertEquals(LocalDateTime.now().getDayOfWeek(), myClient.getCreatedAt().getDayOfWeek());
+    }
+    @Test
+    public void all_returnsAllInstancesOfClient_true() {
+        Client firstClient = new Client("Bill Odida");
+        Client secondClient = new Client("Brian Marete");
+        assertEquals(true, Client.all().contains(firstClient));
+        assertEquals(true, Client.all().contains(secondClient));
+    }
 }
