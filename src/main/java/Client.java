6b5be6eea4e829871faa96265dceb18bb5bebb1b
wolf <billodida420@gmail.com>
2019-04-23 20:56:41 +0300

    test to assign IDs

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -6,11 +6,13 @@ public class Client {
     private String mDescription;
     private LocalDateTime mCreatedAt;
     private static List<Client> instances = new ArrayList<Client>();
+    private int mId;
 
     public Client(String description) {
         mDescription = description;
         mCreatedAt = LocalDateTime.now();
         instances.add(this);
+        mId = instances.size();
     }
 
     public String getDescription() {
@@ -23,5 +25,12 @@ public class Client {
 
     public static List<Client> all() {
         return instances;
+    }
+
+    public static void clear() {
+        instances.clear();
+    }
+    public int getId() {
+        return mId;
     }
 }
